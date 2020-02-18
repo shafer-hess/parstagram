3e33f903cb4bc5ac75616e18cda688852edd4d3f
Shafer <shafer.hess01@gmail.com>
2020-02-17 19:23:34 -0500

    Added CameraViewController, CameraViewLayout and login and signup functionality

---
+++ Parstagram/CameraViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CameraViewController.swift
+//  Parstagram
+//
+//  Created by Shafer Hess on 2/17/20.
+//  Copyright © 2020 Shafer Hess. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+
+    // CameraViewController Outlets
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSubmit(_ sender: Any) {
+    
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
