8845997719cd68abc0f72a35d4083b70985f3e43
Shafer <shafer.hess01@gmail.com>
2020-02-17 21:46:23 -0500

    Added camera picker  privacy settings and camera window

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -7,8 +7,9 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // CameraViewController Outlets
     @IBOutlet weak var imageView: UIImageView!
@@ -25,7 +26,27 @@ class CameraViewController: UIViewController {
     }
     
     @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+
+        if(UIImagePickerController.isSourceTypeAvailable(.camera)) {
+                picker.sourceType = .camera
+        } else {
+                picker.sourceType = .photoLibrary
+        }
         
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        dismiss(animated: true, completion: nil)
     }
     
     /*
